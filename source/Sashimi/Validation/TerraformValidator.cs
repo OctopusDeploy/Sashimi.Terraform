c71f05c1dd8763a32a37c00f2d4dea21e2c0b37a
Matthew Casperson <matthewcasperson@gmail.com>
2021-08-05 13:54:21 +1000

    Clean up using statements

--- source/Sashimi/Validation/TerraformValidator.cs
+++ source/Sashimi/Validation/TerraformValidator.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using FluentValidation;
 using Newtonsoft.Json.Linq;
 using Octopus.CoreParsers.Hcl;
-using Octopus.TinyTypes;
 using Sashimi.Server.Contracts;
 using Sashimi.Server.Contracts.ActionHandlers.Validation;
 using Sashimi.Server.Contracts.CloudTemplates;
